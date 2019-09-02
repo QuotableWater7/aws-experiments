efd04841ce3e9826afc52ea84cb68ed09fb80e50
JT <jt@buildingconnected.com>
2019-09-02 14:35:50 -0700

    [misc] Add todo list; working s3 command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
+const aws = require('aws-sdk')
+
 async function main() {
-  console.log('hello')
+  const s3 = new aws.S3()
+
+  await s3.createBucket({ Bucket: 'jt-bucket-123' }).promise()
+
+  console.log('done.')
 }
 
 main().catch(error => {
